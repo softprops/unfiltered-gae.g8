fe11700715ccf5a503a523a27e914fcfdac8f414
Nathan Hamblen <nathan@technically.us>
2010-08-16 11:42:14 +0800

    use plugin for template testing

--- project/build/project.scala
+++ project/build/project.scala
@@ -1,22 +1,3 @@
 import sbt._
 
-class UnfilteredTemplate(info: ProjectInfo) extends DefaultProject(info) {
-  def templateSourcePath = (mainSourcePath / "g8") ##
-  def templateSources = descendents(templateSourcePath, "*")
-
-  def defaultProperties = templateSourcePath / "default.properties"
-
-  def templateOutput = outputPath / "g8"
-
-  lazy val writeTemplates = task { None } dependsOn (
-    templateSources.get.filter(!_.isDirectory).map { in =>
-      templateTask(in, Path.fromString(templateOutput, in.relativePath))
-    }.toSeq: _*
-  )
-  def templateTask(in: Path, out: Path) = fileTask(out from in) {
-    FileUtilities.write(out.asFile, log) { write =>
-      write write scala.io.Source.fromFile(in.asFile).mkString("")
-      None
-    }
-  }
-}
+class UnfilteredTemplate(info: ProjectInfo) extends DefaultProject(info) with giter8.Template
