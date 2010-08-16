8319f10b589822cb4f008255a12f6fef975a9279
Nathan Hamblen <nathan@technically.us>
2010-08-15 23:42:14 -0400

    use plugin for template testing

--- src/main/g8/project/build/Project.scala
+++ src/main/g8/project/build/Project.scala
@@ -1,10 +1,10 @@
 import sbt._
 
 class Project(info: ProjectInfo) extends DefaultProject(info) {
-  val uf_version = "$ufversion$"
+  val uf_version = "$unfiltered_version$"
   
   // unfiltered
-  lazy val uf = "net.databinder" %% "$ufmodule$" % uf_version
+  lazy val uf = "net.databinder" %% "$unfiltered_module$" % uf_version
 
   // testing
   lazy val uf_spec = "net.databinder" %% "unfiltered-spec" % uf_version % "test"
