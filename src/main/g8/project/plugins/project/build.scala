50a9bd4c26eece13e27b6771067760d3369dd354
softprops <d.tangren@gmail.com>
2011-09-04 23:48:13 -0400

    unused code

--- src/main/g8/project/plugins/project/build.scala
+++
@@ -1,8 +0,0 @@
-/*import sbt._
-
-object Plugins extends Build {
-  lazy val root = Project("root", file(".")) dependsOn(
-    uri("git://github.com/eed3si9n/sbt-appengine.git")
-    //uri("git://github.com/siasia/xsbt-web-plugin.git")
-  )
-}*/
